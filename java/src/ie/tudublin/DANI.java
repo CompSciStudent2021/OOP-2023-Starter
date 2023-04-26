023e2e9bd40efcbc1f8e3909bcf9f15acff0560e
Ryan Francis <C21730391@mytudublin.ie>
2023-04-26 16:52:26 +0100

    Who knows?

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,7 +6,61 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
+	public void loadFile() {
+		loadStrings("filename.txt"); // Load a text file into a String array
+		split(line, ' '); // Split a string into an array of words
+		w.replaceAll("[^\\w\\s]",""); // Remove punction characters
+		s.toLowerCase() // Convert a string to lower case 
+	}
+
+	public void printModel() {
+		for (int i = 0; i < words.size(); i++) {
+			Word w = words.get(i);
+			println(w);
+		}
+	}
 	
+	public string writeSonnet() {
+		// make a copy of the arraylist
+		ArrayList<Word> copy = new ArrayList<Word>();
+		for (Word w : words) {
+			Word c = new Word(w);
+			copy.add(c);
+		}
+		// Generate first word
+		int index = (int) random(copy.size());
+		Word current = copy.get(index);
+		String poem = current.getWord() + " ";
+		int line = 0;
+		while (line < 14) {
+			// Find the word
+			current = copy.get(index);
+			poem = poem + current.getWord() + " ";
+			// Find the next word
+			if (current.getFollows().size() == 0) {
+				break;
+			}
+			index = (int) random(current.getFollows().size());
+			Follow f = current.getFollows().get(index);
+			String next = f.getWord();
+			// Remove the word from the list
+			copy.remove(current);
+			// Next word
+			Word nextWord = null;
+			for (Word w : copy) {
+				if (w.getWord().equals(next)) {
+					nextWord = w;
+					break;
+				}
+			}
+			if (nextWord == null) {
+				break;
+			}
+			current = nextWord;
+			line++;
+		}
+		return poem;
+	}
 
 	public void settings() {
 		size(1000, 1000);
--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -22,5 +22,36 @@ public class Word {
         this.follows = follows;
     }
 
-    
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public void addFollow(Follow f)
+    {
+        follows.add(f);
+    }
+
+    public void render()
+    {
+        for(Follow f:follows)
+        {
+            f.render();
+        }
+    }
+
+    public String toString()
+    {
+        return word + "\t" + follows;
+    }
+
+    public void findFollow() {
+        for (int i = 0; i < sonnet.length - 1; i++) {
+            String word = sonnet[i];
+            String next = sonnet[i + 1];
+            if (word.equals(this.word)) {
+                Follow f = new Follow(next);
+                addFollow(f);
+            }
+        }
+    }
 }
